5ee55d8a94f8862734cd4c61e76d5bcc3070fef9
ahmetpalavan <ahmetpalavann@gmail.com>
2023-03-15 15:42:42 +0300

    carousel added

--- food/next.config.js
+++ food/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["html.design"],
+    domains: ["html.design","raw.githubusercontent.com"],
   },
 }
 
---
+++ food/pages/home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Carousel from "../../components/Carousel";
+
+const index = () => {
+  return (
+    <div>
+      <Carousel />
+    </div>
+  );
+};
+
+export default index;
