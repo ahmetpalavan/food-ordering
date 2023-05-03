ccc0c81a50a9f3fa71ba69e0a88313cae2e8343a
ahmetpalavan <ahmetpalavann@gmail.com>
2023-05-04 01:50:15 +0300

    added github sign in

--- food/pages/auth/login.jsx
+++ food/pages/auth/login.jsx
@@ -5,8 +5,11 @@ import Input from "../../components/form/Input";
 import Title from "../../components/ui/Title";
 import loginSchema from "../../schema/loginSchema";
 import Link from "next/link";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const login = () => {
+  const { data: session } = useSession();
+  console.log(session, "session123");
   const onSubmit = async (values, actions) => {
     await new Promise((r) => setTimeout(r, 3000));
     actions.resetForm();
@@ -44,15 +47,17 @@ const login = () => {
     <div className="container mx-auto">
       <form onSubmit={handleSubmit} className="flex flex-col items-center my-20 w-1/2 mx-auto">
         <Title addClass="text-[40px] text-center md:w-1/2 w-full">Login</Title>
-        <Grid className="flex flex-col gap-y-3 w-full">
+        <Grid mt={3} className="flex flex-col gap-y-3 w-full">
           {inputs.map((input) => (
             <Input key={input.id} {...input} onChange={handleChange} onBlur={handleBlur} />
           ))}
           <Grid className="flex flex-col w-full gap-y-4 mt-6">
-            <Button className="btn-primary" onClick={handleSubmit}>
+            <Button type="submit" className="btn-primary" onClick={handleSubmit}>
               Login
             </Button>
-            <Button className="btn-primary !bg-secondary">Github</Button>
+            <Button onClick={() => signIn("github")} type="button" className="btn-primary !bg-secondary">
+              Github
+            </Button>
             <Link href="/auth/register">
               Don't have an account? <span className="text-primary">Register</span>
             </Link>
