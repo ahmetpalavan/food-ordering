7d5647b1da012c4f04b9ee5d91f291f8cbb4f05f
ahmetpalavan <ahmetpalavann@gmail.com>
2023-05-04 01:49:52 +0300

    add next auth

---
+++ food/pages/api/auth/[...nextauth].js
@@ -0,0 +1,20 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "../../../util/mongo";
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    // ...add more providers here
+  ],
+  // A database is optional, but required to persist accounts in a database
+  pages: {
+    signIn: "/auth/login",
+  },
+  adapter: MongoDBAdapter(clientPromise),
+});
